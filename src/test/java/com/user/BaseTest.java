73442668d4090b13acf9a58476a1a2641858ee01
Tayade <Harshal.Tayade@t-systems.com>
2023-08-28 14:37:45 +0530

    update

---
+++ src/test/java/com/user/BaseTest.java
@@ -0,0 +1,18 @@
+package com.user;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+
+public class BaseTest {
+    @Configuration
+    static class TestConfig {
+
+    }
+}
---
+++ src/test/java/com/user/controller/UserControllerTest.java
@@ -0,0 +1,17 @@
+package com.user.controller;
+
+import com.user.BaseTest;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+public class UserControllerTest extends BaseTest {
+    private MockMvc mockMvc;
+//    @Order(1)
+//    @Test
+//    @SneakyThrows
+//    public void createWithFields() {
+//        mockMvc.perform(post("http://localhost:7070/user")
+//                .contentType(APPLICATION_JSON)
+//                .accept(APPLICATION_JSON));
+//    }
+}
